a44205e0bb598b16f80472c652bcc3cfb2783da8
Alexander Ryazanov <alryaz@xavux.com>
2021-08-27 01:27:00 +0300

    Revert "feat: added experimental circumvention of 'platform taking too long to set up'"
    This reverts commit 5620172973252e2d34edb3f8e2789c13a870710b.

--- custom_components/energosbyt_plus/_base.py
+++ custom_components/energosbyt_plus/_base.py
@@ -37,7 +37,6 @@ from homeassistant.const import (
     CONF_DEFAULT,
     CONF_SCAN_INTERVAL,
     CONF_USERNAME,
-    EVENT_HOMEASSISTANT_START,
 )
 from homeassistant.helpers import entity_platform
 from homeassistant.helpers.entity import Entity
@@ -145,11 +144,7 @@ async def async_register_update_delegator(
         if len(update_delegators) != len(SUPPORTED_PLATFORMS):
             return
 
-        async def _async_started_callback(*_):
-            _LOGGER.debug(f"Received start event for entry {entry_id}")
-            await async_refresh_api_data(hass, config_entry)
-
-        hass.bus.async_listen_once(EVENT_HOMEASSISTANT_START, _async_started_callback)
+        await async_refresh_api_data(hass, config_entry)
 
 
 async def async_refresh_api_data(hass: HomeAssistantType, config_entry: ConfigEntry):
